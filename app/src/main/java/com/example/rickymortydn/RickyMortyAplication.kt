40856df68555a5136215560eea16a279a73daf29
David Navarro <davidnavarrom3@gmail.com>
2023-06-18 22:36:53 +0200

    Added DaggerHilt & Implementations Added Mapper Added logic to Repository Added Internet Permission Added ResourceState Added Application & name Deleted RickyMortyApiClient.kt Changed Service

---
+++ app/src/main/java/com/example/rickymortydn/RickyMortyAplication.kt
@@ -0,0 +1,7 @@
+package com.example.rickymortydn
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickyMortyApplication  : Application()
--- app/src/main/java/com/example/rickymortydn/domain/characters/CharactersUseCase.kt
+++ app/src/main/java/com/example/rickymortydn/domain/characters/CharactersUseCase.kt
@@ -2,10 +2,12 @@ package com.example.rickymortydn.domain.characters
 
 import com.example.rickymortydn.domain.characters.repository.CharacterRepository
 import com.example.rickymortydn.models.CharacterModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class CharacterUseCase(private val characterRepository: CharacterRepository) {
-
-    suspend fun getCharacters(): List<CharacterModel> {
-        return characterRepository.getCharacters()
-    }
+class CharacterUseCase @Inject constructor(
+    private val characterRepository: CharacterRepository
+) {
+    suspend operator fun invoke(): Flow<List<CharacterModel.CharacterResult>> =
+        characterRepository.getCharacters()
 }
