9cb8eed654b733266ec1df16e277b9034c4e7133
David Navarro <davidnavarrom3@gmail.com>
2023-06-18 22:53:58 +0200

    Changed strings to pick correctly data

--- app/src/main/java/com/example/rickymortydn/models/common/constants/AppConstants.kt
+++ app/src/main/java/com/example/rickymortydn/models/common/constants/AppConstants.kt
@@ -1,6 +1,6 @@
 package com.example.rickymortydn.models.common.constants
 
 const val BASE_URL = "https://rickandmortyapi.com/api/"
-const val CHARACTERS = "characters"
+const val CHARACTER = "character"
 const val EPISODES = "episode"
 const val LOCATIONS = "location"
