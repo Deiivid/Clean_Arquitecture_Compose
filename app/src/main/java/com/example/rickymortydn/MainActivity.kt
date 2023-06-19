c0603d1f36452474f2582aa2c695aa1fa4a6de2b
Deiivid <60486280+Deiivid@users.noreply.github.com>
2023-06-19 23:25:18 +0200

    Changes on CharacterList add CharacterList ADD navigation

--- app/src/main/java/com/example/rickymortydn/MainActivity.kt
+++ app/src/main/java/com/example/rickymortydn/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.rickymortydn.ui.character.screen.CharacterListScreen
 import com.example.rickymortydn.ui.character.viewmodel.CharactersViewModel
 import com.example.rickymortydn.ui.theme.RickyMortyDNTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,11 +26,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             RickyMortyDNTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyScreenContent()
+                /* Surface(
+                     modifier = Modifier.fillMaxSize(),
+                     color = MaterialTheme.colorScheme.background
+                 ) {
+                     MyScreenContent()
+                 }*/
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "characterList") {
+                    composable("characterList") {
+                        CharacterListScreen(navController = navController)
+                    }
+                    composable("character_detail") {
+
+                    }
                 }
             }
         }
