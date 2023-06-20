f3b912dc7e37539b8dfa77a2f67ab52becfa53f1
David Navarro <davidnavarrom3@gmail.com>
2023-06-20 18:12:50 +0200

    moved to commons navigation

--- app/src/main/java/com/example/rickymortydn/MainActivity.kt
+++ app/src/main/java/com/example/rickymortydn/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.rickymortydn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.rickymortydn.ui.common.navigation.navgraph.SetupNavGraph
+import androidx.navigation.navArgument
+import com.example.rickymortydn.models.common.navigation.Routes
+import com.example.rickymortydn.ui.character.screens.CharactersListScreen
+import com.example.rickymortydn.ui.character.viewmodel.CharactersViewModel
 import com.example.rickymortydn.ui.theme.RickyMortyDNTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,26 +31,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             RickyMortyDNTheme {
                 // A surface container using the 'background' color from the theme
-                /*  Surface(
-                      modifier = Modifier.fillMaxSize(),
-                      color = MaterialTheme.colorScheme.background
-                  ) {
-                         val navigationController = rememberNavController()
-                         NavHost(
-                             navController = navigationController,
-                             startDestination = Routes.CharacterList.route
-                         ) {
-                             composable(Routes.CharacterList.route) { CharactersListScreen(navigationController) }
-                         }
-                     */
-
-                    val navController = rememberNavController()
-                SetupNavGraph(navController)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navigationController = rememberNavController()
+                    NavHost(
+                        navController = navigationController,
+                        startDestination = Routes.CharacterList.route
+                    ) {
+                        composable(Routes.CharacterList.route) { CharactersListScreen(navigationController) }
+                    }
                 }
             }
         }
     }
-
+}
 
 
 @Preview(showBackground = true)
