f2c5640c6ebfd2a265563159d85d40bf904583e0
David Navarro <davidnavarrom3@gmail.com>
2023-09-16 09:07:26 +0200

    Updated gradle

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+    }
+}
+
