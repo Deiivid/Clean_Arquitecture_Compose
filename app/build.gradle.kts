b9dd0da22396c3f3d27d12cac839b051a6655468
Deiivid <60486280+Deiivid@users.noreply.github.com>
2023-09-16 09:28:55 +0200

    Upgraded gradel dependencies out of build.gradle.kts(app) to own folder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,49 +49,7 @@ android {
         }
     }
 }
-//Dependencies Versions
-val compose_version = "1.1.1"
-val hilt_version = "2.45"
-val coil_version = "1.3.2"
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.4.1")
-    implementation("androidx.compose.ui:ui:$compose_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha02")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_version")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // Compose
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0-alpha02")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
-    implementation("androidx.navigation:navigation-compose:2.5.3")
-
-    implementation("com.google.dagger:hilt-android:2.39.1")
-    kapt("com.google.dagger:hilt-compiler:2.39.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-
-    // Dependencia de MockK
-    testImplementation("io.mockk:mockk:1.12.0")
-
-    // Dependencias relacionadas con las pruebas y Kotlin coroutines
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
-
-    // Coil images
-    implementation("io.coil-kt:coil-compose:$coil_version")
-
-    // Lottie
-    implementation("com.airbnb.android:lottie-compose:4.2.0")
+apply(from = "dependencies.gradle")
 }
