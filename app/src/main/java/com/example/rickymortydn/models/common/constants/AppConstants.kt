6481a1ba8dffb8ea1a2a634cde33ff2b02adffff
dnavarro <dnavarro@hiberus.com>
2023-10-25 16:07:35 +0200

    Fix: Project namespace

--- app/src/main/java/com/example/rickymortydn/models/common/constants/AppConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rickymortydn.models.common.constants
-
-const val BASE_URL = "https://rickandmortyapi.com/api/"
-const val CHARACTER = "character"
-const val EPISODES = "episode"
-const val LOCATIONS = "location"
