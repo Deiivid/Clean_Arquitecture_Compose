7279c69f24689e0ff9ff1bfff544ad75fa407906
Deiivid <60486280+Deiivid@users.noreply.github.com>
2023-10-25 16:07:35 +0200

    Fix: Project namespace

--- app/src/main/java/com/example/rickymortydn/domain/characters/repository/CharacterRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.rickymortydn.domain.characters.repository
-
-import com.example.rickymortydn.models.CharacterModel
-import kotlinx.coroutines.flow.Flow
-
-interface CharacterRepository {
-    suspend fun getCharacters(): Flow<List<CharacterModel.CharacterResult>>
-}
--- app/src/main/java/com/example/rickymortydn/ui/common/states/ResourceState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.rickymortydn.ui.common.states
-
-sealed class ResourceState<T> {
-    object Idle : ResourceState<Void>()
-    data class Loading<String>(val data: String) : ResourceState<String>()
-    data class Error<Throwable>(val throwable: Throwable) : ResourceState<Throwable>()
-    data class Success<T>(val data: T) : ResourceState<T>()
-}
