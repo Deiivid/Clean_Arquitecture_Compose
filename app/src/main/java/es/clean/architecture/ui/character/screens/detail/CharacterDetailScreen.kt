3609b80a5c08535b033b9dede2efd53654f1c70b
X\MisterX <davidnavarrom3@gmail.com>
2023-10-26 23:15:56 +0200

    Added visual changes for Detail

--- app/src/main/java/es/clean/architecture/ui/character/screens/detail/CharacterDetailScreen.kt
+++ app/src/main/java/es/clean/architecture/ui/character/screens/detail/CharacterDetailScreen.kt
@@ -1,27 +1,54 @@
 package es.clean.architecture.ui.character.screens.detail
 
+import android.graphics.fonts.Font
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Calculate
+import androidx.compose.material.icons.filled.Eco
+import androidx.compose.material.icons.filled.GraphicEq
+import androidx.compose.material.icons.filled.LocalHospital
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.DarkMode
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.EmojiPeople
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.twotone.AccountCircle
+import androidx.compose.material.icons.twotone.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,6 +56,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
+import es.clean.architecture.R
 import es.clean.architecture.models.CharacterModel
 import es.clean.architecture.models.createCharacterResult
 import es.clean.architecture.ui.common.navigation.Routes
@@ -43,62 +71,187 @@ fun CharacterDetailScreen(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background,
     ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top
         ) {
-            Column(
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .height(400.dp)
+                    .clip(RoundedCornerShape(bottomEnd =16.dp, bottomStart =16.dp))
+                    .background(Color.LightGray),
+                contentAlignment = Alignment.Center
             ) {
-                Text(
-                    character.name,
-                    modifier = Modifier.align(CenterHorizontally),
-                    style = MaterialTheme.typography.titleLarge,
-                    fontSize = 20.sp
+                Image(
+                    painter = rememberImagePainter(data = character.image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 1.dp)
+                        .background(Color.White)
                 )
-                Spacer(modifier = Modifier.height(6.dp))
-                Text(
-                    character.gender,
-                    modifier = Modifier.align(CenterHorizontally),
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontSize = 14.sp
+                /*Image(
+                    painter = painterResource(R.drawable.test_image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 1.dp)
+                        .background(Color.White)
+                )*/
+            }
 
-                )
-                Spacer(modifier = Modifier.height(6.dp))
+            Column(
+                modifier = Modifier.padding(16.dp),
+            ) {
                 Box(
                     modifier = Modifier
-                        .padding(6.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(Color.LightGray)
-                        .align(CenterHorizontally)
-                        .size(height = 600.dp, width = 500.dp)
-                        .clickable {
-                            navController.navigate(Routes.CharacterList.route)
-                        },
-                    contentAlignment = Alignment.Center,
-                    content = {
-                        Image(
-                            painter = rememberImagePainter(data = character.image),
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(Color.Transparent)
+                        .shadow(elevation = 4.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Box(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .clip(RoundedCornerShape(16.dp))
-                                .background(Color.White)
-                        )
-                        /*Image(
-                            painter = painterResource(R.drawable.ic_test),
-                            contentDescription = null,
-                            contentScale = ContentScale.Inside,
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(6.dp))
+                                .shadow(elevation = 2.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(60.dp)
+                                    .padding(6.dp),
+                                verticalAlignment = CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+
+                            ) {
+                                Icon(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .align(CenterVertically),
+                                    imageVector = Icons.Outlined.DarkMode,
+                                    contentDescription = null,
+                                    tint = Color.Black,
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(
+                                    character.name,
+                                    style = MaterialTheme.typography.titleLarge,
+                                    fontSize = 20.sp,
+                                    textAlign = TextAlign.Center
+
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Box(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .clip(RoundedCornerShape(16.dp))
-                                .background(Color.White)
-                        )*/
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(6.dp))
+                                .shadow(elevation = 2.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(60.dp)
+                                    .padding(6.dp),
+                                verticalAlignment = CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Outlined.EmojiPeople,
+                                    contentDescription = null,
+                                    tint = Color.Black
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(
+                                    character.gender,
+                                    style = MaterialTheme.typography.titleLarge,
+                                    fontSize = 20.sp,
+                                    textAlign = TextAlign.Center
+
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(6.dp))
+                                .shadow(elevation = 2.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(60.dp)
+                                    .padding(6.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.GraphicEq,
+                                    contentDescription = null,
+                                    tint = Color.Black
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(
+                                    character.species,
+                                    style = MaterialTheme.typography.titleLarge,
+                                    fontSize = 20.sp,
+                                    textAlign = TextAlign.Center
+
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(6.dp))
+                                .shadow(elevation = 2.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(60.dp)
+                                    .padding(6.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                val iconColor = if (character.status != "Alive") {
+                                    Color.Red
+                                } else {
+                                    Color.Black
+                                }
+                                Icon(
+                                    imageVector = Icons.Default.LocalHospital,
+                                    contentDescription = null,
+                                    tint = iconColor
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(
+                                    character.status,
+                                    style = MaterialTheme.typography.titleLarge,
+                                    fontSize = 20.sp,
+                                    textAlign = TextAlign.Center
+
+                                )
+                            }
+                        }
                     }
-                )
+                }
             }
         }
     }
