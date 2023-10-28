df8558cd2fa3a8acf383c13f0d33bacbaf6618ef
Deiivid <60486280+Deiivid@users.noreply.github.com>
2023-10-28 06:38:55 +0200

    Fix: ViewModel & ResourceState

--- app/src/main/java/es/clean/architecture/ui/common/states/ResourceState.kt
+++ app/src/main/java/es/clean/architecture/ui/common/states/ResourceState.kt
@@ -1,8 +1,8 @@
 package es.clean.architecture.ui.common.states
 
 sealed class ResourceState<T> {
-    object Idle : ResourceState<Void>()
-    data class Loading<String>(val data: String) : ResourceState<String>()
-    data class Error<Throwable>(val throwable: Throwable) : ResourceState<Throwable>()
+     class Idle<T> : ResourceState<T>()
+     class Loading<T>: ResourceState<T>()
+    data class Error<T>(val throwable: Throwable) : ResourceState<T>()
     data class Success<T>(val data: T) : ResourceState<T>()
 }
