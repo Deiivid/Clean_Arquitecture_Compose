d10bf9d849349f0128bd5fe5445ac76f8c4c9b85
David Navarro <davidnavarrom3@gmail.com>
2023-11-01 10:01:15 +0100

    Add:view changes

--- app/src/main/java/es/clean/architecture/ui/views/characters/viewmodel/CharactersViewModel.kt
+++ app/src/main/java/es/clean/architecture/ui/views/characters/viewmodel/CharactersViewModel.kt
@@ -2,45 +2,43 @@ package es.clean.architecture.ui.views.characters.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import es.clean.architecture.domain.characters.CharacterUseCase
 import es.clean.architecture.domain.characters.models.character.RickyMortyCharacter
 import es.clean.architecture.ui.common.states.ResourceState
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import java.io.InvalidObjectException
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersViewModel @Inject constructor(
-    private val characterUseCase: CharacterUseCase
+    private val getCharacterUseCase: CharacterUseCase
 ) : ViewModel() {
-    private val _charactersSearched by lazy {
-        MutableStateFlow<ResourceState<List<RickyMortyCharacter.Character>>>(
-            ResourceState.Idle()
-        )
-    }
-    val charactersSearched: StateFlow<ResourceState<List<RickyMortyCharacter.Character>>>
+    private val _charactersSearched by lazy { MutableStateFlow<ResourceState<*>>(ResourceState.Idle) }
+    val charactersSearched: StateFlow<ResourceState<*>>
         get() = _charactersSearched
 
-    fun fetchCharacters() {
-        /*        _charactersSearched.update {
-                    ResourceState.Loading()
-                }
-          */      viewModelScope.launch(Dispatchers.IO) {
-
-            characterUseCase().collectLatest { characters ->
-                _charactersSearched.update {
-                    if (characters.isNotEmpty())
-                        ResourceState.Success(characters)
-                    else
-                        ResourceState.Error(InvalidObjectException("Characters not found"))
-                }
+    fun fetchCharacters(): Flow<PagingData<RickyMortyCharacter.Character>> =
+        getCharacterUseCase()
+            .catch { error ->
+                _charactersSearched.update { ResourceState.Error(error) }
             }
-        }
-    }
+            .onStart {
+                _charactersSearched.update { ResourceState.Loading("") }
+            }
+            .map { pagingData ->
+                delay(1000)
+                _charactersSearched.update { ResourceState.Success("") }
+                pagingData
+            }
+            .cachedIn(viewModelScope)
 }
+
