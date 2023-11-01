187c0c026a6e4f6ecdac77448ea21df4f979b8c4
Deiivid <60486280+Deiivid@users.noreply.github.com>
2023-11-01 21:34:34 +0100

    add domain logic for paging

--- app/src/main/java/es/clean/architecture/domain/characters/CharactersUseCase.kt
+++ app/src/main/java/es/clean/architecture/domain/characters/CharactersUseCase.kt
@@ -1,13 +1,8 @@
 package es.clean.architecture.domain.characters
 
-import es.clean.architecture.domain.characters.repository.CharacterRepository
-import es.clean.architecture.models.CharacterModel
-import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
-
-class CharacterUseCase @Inject constructor(
+/*class CharacterUseCase @Inject constructor(
     private val characterRepository: CharacterRepository
 ) {
     suspend operator fun invoke(): Flow<List<CharacterModel.CharacterResult>> =
         characterRepository.getCharacters()
-}
+}*/
