6180d350a61cd8f2bbf36ff7ff8f3994fad7b06e
David Navarro <davidnavarrom3@gmail.com>
2023-11-02 14:07:13 +0100

    Add:paging

--- app/src/main/java/es/clean/architecture/data/di/NetworkModule.kt
+++ app/src/main/java/es/clean/architecture/data/di/NetworkModule.kt
@@ -5,10 +5,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import es.clean.architecture.data.characters.remote.implement.CharacterRepositoryImpl
-import es.clean.architecture.data.service.RickyMortyService
-import es.clean.architecture.domain.characters.repository.characters.CharacterRepository
 import es.clean.architecture.data.common.BASE_URL
+import es.clean.architecture.data.remote.characters.datastore.RemoteCharacterDataStoreImpl
+import es.clean.architecture.data.remote.characters.interfaces.CharactersDataStore
+import es.clean.architecture.data.remote.characters.repository.CharactersRepository
+import es.clean.architecture.data.service.RickyMortyService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -38,10 +39,15 @@ object NetworkModule {
             .build()
     }
 
+   /* @Provides
+    @Singleton
+    fun provideCharacterRepository(remoteService: RickyMortyService): CharactersDataStore {
+        return RemoteCharacterDataStoreImpl(remoteService)
+    }*/
     @Provides
     @Singleton
-    fun provideCharacterRepository(remoteService: RickyMortyService): CharacterRepository {
-        return CharacterRepositoryImpl(remoteService)
+    fun provideCharacterRepository(remoteService: RickyMortyService): CharactersRepository {
+        return RemoteCharacterDataStoreImpl(remoteService)
     }
 
 }
