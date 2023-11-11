a81883f4b337f6d68a7dad1cae4a67c8cd2a9942
Deiivid <60486280+Deiivid@users.noreply.github.com>
2023-11-11 20:49:19 +0100

    Added filtering for character name

--- app/src/main/java/es/clean/architecture/domain/characters/repository/characters/AllCharactersUseCase.kt
+++ app/src/main/java/es/clean/architecture/domain/characters/repository/characters/AllCharactersUseCase.kt
@@ -3,12 +3,13 @@ package es.clean.architecture.domain.characters.repository.characters
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import es.clean.architecture.data.remote.characters.paging.CharactersPaging
+import es.clean.architecture.data.remote.characters.repository.CharactersRepository
 import javax.inject.Inject
 
 open class AllCharactersUseCase @Inject constructor(
-    private val pagingSource: CharactersPaging
+    private val pagingSource: CharactersRepository
 ) {
-    operator fun invoke(limit: Int) = Pager(
+    operator fun invoke(query: String? = null, limit: Int) = Pager(
         // aquí el tamaño de página se comporta de forma curiosa,
         // la primera petición (es así) multiplica el pagesize * 3
         config = PagingConfig(
@@ -16,7 +17,7 @@ open class AllCharactersUseCase @Inject constructor(
             prefetchDistance = limit / 2 // aquí recolectamos a la mitad, asi va más "fluido"
         ),
         pagingSourceFactory = {
-            pagingSource
+            CharactersPaging(pagingSource, query)
         }
     ).flow // Si nos fijamos creamos un flow directamente desde el caso de uso.
 }
