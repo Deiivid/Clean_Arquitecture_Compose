7b50d54a297b040747bc14c8b268e8ccd6361f83
dnavarro <dnavarro@hiberus.com>
2023-11-15 17:47:12 +0100

    Add image & new loading

--- app/src/main/java/es/clean/architecture/ui/views/characters/screens/list/CharacterListScreen.kt
+++ app/src/main/java/es/clean/architecture/ui/views/characters/screens/list/CharacterListScreen.kt
@@ -73,8 +73,12 @@ fun CharactersListScreen(
 
     when (characters.loadState.refresh) {
         is LoadState.Loading -> {
-            // Mostrar animación de carga
-            LottieProgressBar()
+            if (searchQuery != "") {
+                LottieSearch()
+            } else {
+                // Mostrar animación de carga
+                LottieProgressBar()
+            }
         }
 
         is LoadState.NotLoading -> {
@@ -277,13 +281,24 @@ fun LottieProgressBar() {
 }
 
 @Composable
+fun LottieSearch() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loadingsearch))
+    val progress by animateLottieCompositionAsState(composition,speed = 2f)
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+        modifier = Modifier.fillMaxSize()
+    )
+
+}
+@Composable
 fun LottieErrorState() {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.cryricky))
     val progress by animateLottieCompositionAsState(composition)
     LottieAnimation(
         composition = composition,
         progress = { progress },
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
     )
 
 }
