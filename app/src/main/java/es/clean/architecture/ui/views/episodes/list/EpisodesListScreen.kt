4d6d58bb6c72f1139298be8e62250af8a5fca3d4
David Navarro <davidnavarrom3@gmail.com>
2023-11-21 13:06:53 +0100

    Fixed episodes look

--- app/src/main/java/es/clean/architecture/ui/views/episodes/list/EpisodesListScreen.kt
+++ app/src/main/java/es/clean/architecture/ui/views/episodes/list/EpisodesListScreen.kt
@@ -1,12 +1,28 @@
 package es.clean.architecture.ui.views.episodes.list
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmarks
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.Web
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,9 +32,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +57,10 @@ import es.clean.architecture.domain.episodes.models.createEpisodesResult
 import es.clean.architecture.ui.common.navigation.routes.Routes
 import es.clean.architecture.ui.common.EPISODE_OBJECT
 import es.clean.architecture.ui.views.episodes.viewmodel.EpisodesViewModel
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,7 +91,10 @@ fun EpisodesListScreen(
         is LoadState.NotLoading -> {
             // Si no está cargando, muestras los personajes en el LazyColumn
             Scaffold(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.app_background)),
+
                 topBar = {
                     CenterAlignedTopAppBar(
                         modifier = Modifier
@@ -103,37 +132,45 @@ fun EpisodesListScreen(
                     )
                 }
             ) { paddingValues ->
-                LazyColumn(
+                Box(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(paddingValues)
+                        .background(
+                            colorResource(id = R.color.app_background)
+                        )
+                        .padding(bottom = 10.dp)
                 ) {
-                    items(
-                        count = episodes.itemCount,
-                        key = episodes.itemKey { episode -> episode.id }
-                    ) { episodesIndex ->
-                        episodes[episodesIndex]?.let { item ->
-                            EpisodesItem(
-                                episodes = item,
-                            ) { currentEpisode ->
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) {
+                        items(
+                            count = episodes.itemCount,
+                            key = episodes.itemKey { episode -> episode.id }
+                        ) { episodesIndex ->
+                            episodes[episodesIndex]?.let { item ->
+                                EpisodesItem(
+                                    episodes = item,
+                                ) { currentEpisode ->
 
-                                navController.currentBackStackEntry?.savedStateHandle?.set(
-                                    EPISODE_OBJECT,
-                                    value = currentEpisode
-                                )
-                                navController.navigate(Routes.EpisodeDetailScreen.route)
-
-                                /* scope.launch {
+                                    /* navController.currentBackStackEntry?.savedStateHandle?.set(
+                                     EPISODE_OBJECT,
+                                     value = currentEpisode
+                                 )
+                                 navController.navigate(Routes.EpisodeDetailScreen.route)
+ */
+                                    /* scope.launch {
                                      withContext(Dispatchers.Main){
                                          Toast.makeText(context, "Personaje: ${currentCharacter.name}", Toast.LENGTH_LONG).show()
                                      }
                                  }
                              }*/
+                                }
                             }
                         }
                     }
                 }
-
             }
         }
 
@@ -147,8 +184,28 @@ fun EpisodesListScreen(
 @Composable
 fun EpisodesItem(
     episodes: RickyMortyEpisodesModel.Episode,
-    onItemClick: (rickyMortyEpisode: RickyMortyEpisodesModel.Episode) -> Unit
+    onItemClick: (rickyMortyEpisode: RickyMortyEpisodesModel.Episode) -> Unit,
 ) {
+    val context = LocalContext.current // Obtener el Context
+
+    val scale = remember { Animatable(1f) }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1.1f, // Escala hasta un 10% más grande
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 500, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+    }
+
+    var pressed by remember { mutableStateOf(false) }
+
+    val scaleRow by animateFloatAsState(
+        targetValue = if (pressed) 1.1f else 1f,
+        animationSpec = tween(durationMillis = 100), label = "Row"
+    )
+
     Surface(
         shape = RoundedCornerShape(16.dp),
         color = Color(0xFFDAE1E7),
@@ -156,12 +213,12 @@ fun EpisodesItem(
             .clickable {
                 onItemClick(episodes)
             }
-            .height(165.dp)
+            .height(185.dp)
             .padding(10.dp),
         shadowElevation = 10.dp
     ) {
         Row(
-            modifier = Modifier.padding(bottom = 14.dp, top = 8.dp, start = 8.dp, end = 8.dp),
+            modifier = Modifier.padding(bottom = 14.dp, top = 8.dp, start = 8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Column(
@@ -175,39 +232,117 @@ fun EpisodesItem(
                         .wrapContentSize(),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(
-                        text = episodes.name,
-                        fontSize = 12.sp,
-                        style = MaterialTheme.typography.titleLarge,
+                    Row(
                         modifier = Modifier
-                            .padding(vertical = 2.dp, horizontal = 2.dp)
-                            .clip(RoundedCornerShape(8.dp))
-                            .background(Color(0xFFD1D5E1))
-                            .padding(2.dp) // Este padding es para el espacio dentro del fondo, alrededor del texto.
-                    )
+                            .fillMaxWidth()
+                            .scale(scale.value),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+
+                        ) {
+                        Text(
+                            text = episodes.name,
+                            fontSize = 22.sp,
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.titleLarge,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp, horizontal = 10.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                                .background(Color(0xFFEEF7F3))
+                        )
+                    }
                 }
                 Spacer(modifier = Modifier.height(4.dp))
 
-                Text(
-                    text = episodes.name,
-                    fontSize = 20.sp,
-                    style = MaterialTheme.typography.titleLarge,
-                    fontWeight = FontWeight.SemiBold
-                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+
+                    ) {
+                    Icon(
+                        imageVector = Icons.Default.Bookmarks,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+
+                    Text(
+                        text = episodes.episode,
+                        fontSize = 20.sp,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+
 
                 Spacer(modifier = Modifier.height(2.dp))
 
-                Text(text = episodes.airDate)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
 
-                Spacer(modifier = Modifier.height(2.dp))
+                    ) {
+                    Icon(
+                        imageVector = Icons.Default.CalendarMonth,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = "Emision Date:")
+                    Spacer(modifier = Modifier.width(4.dp))
 
-                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = formatEmisionDate(episodes.airDate),
+                        maxLines = 2,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .graphicsLayer {
+                            scaleX = scaleRow
+                            scaleY = scaleRow
+                        }
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onPress = {
+                                    pressed = true
+                                    tryAwaitRelease()
+                                    pressed = false
+                                },
+                                onTap = {
+                                    openUrl(episodes.url, context)
+                                }
+                            )
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+
+                    ) {
+                    Icon(
+                        imageVector = Icons.Default.Language,
+                        contentDescription = null,
+                        tint = Color.DarkGray
+                    )
                     Text(
                         text = episodes.url,
                         fontSize = 12.sp,
+                        maxLines = 2,
                         fontWeight = FontWeight.SemiBold,
-                        style = MaterialTheme.typography.titleMedium
-                    )
+                        color = Color.DarkGray,
+                        style = MaterialTheme.typography.titleMedium,
+
+                        )
                     Spacer(modifier = Modifier.width(4.dp))
                 }
 
@@ -219,6 +354,22 @@ fun EpisodesItem(
     }
 }
 
+fun formatEmisionDate(dateString: String): String {
+    return try {
+        val parser = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val formatter = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        val parsedDate = parser.parse(dateString)
+        formatter.format(parsedDate)
+    } catch (e: Exception) {
+        // Manejar excepción si el formato de fecha es incorrecto
+        dateString
+    }
+}
+
+fun openUrl(url: String, context: Context) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    context.startActivity(intent)
+}
 
 @Composable
 fun LottieProgressBar() {
