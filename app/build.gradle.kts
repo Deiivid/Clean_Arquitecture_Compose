4ed10f2d133f4d7189b12fcee209dc50a57745df
Deiivid <60486280+Deiivid@users.noreply.github.com>
2025-10-27 17:16:17 +0100

    Minor- add fix project add jacoco

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.ksp)
     alias(libs.plugins.detekt)
+    id("jacoco")
 }
 
 android {
@@ -77,6 +78,17 @@ tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
         html.required.set(true)
         xml.outputLocation.set(file("$buildDir/reports/detekt.xml"))
         html.outputLocation.set(file("$buildDir/reports/detekt.html"))
+    }
+}
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.register<JacocoReport>("jacocoDebugReport") {
+    dependsOn("testDebugUnitTest")
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
     }
 }
 
